c7e7165499d2cf2c21d9f40984aae6df63caa80a
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-07 23:50:24 +0300

    Lab 1

---
+++ Object Oriented Programming/Labwork1.java
@@ -0,0 +1,13 @@
+package labwork1;
+public class Labwork1
+{
+    public static void main(String[]args)
+    {
+        Tribe t1 = new Tribe();
+        t1.setName("Aboriginals");
+        t1.setPopulation(10000);
+        t1.setFire_xp(70);
+
+        t1.toConsole();
+    }
+}
