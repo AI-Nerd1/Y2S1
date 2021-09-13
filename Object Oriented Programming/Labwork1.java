0175a5254b8a21f541128843cebc597194f576af
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-13 22:35:18 +0300

    Eclipse1

--- Object Oriented Programming/Labwork1.java
+++ Object Oriented Programming/Labwork1.java
@@ -1,13 +1,19 @@
-package labwork1;
+package Labwork1;
+
 public class Labwork1
 {
-    public static void main(String[]args)
-    {
-        Tribe t1 = new Tribe();
-        t1.setName("Aboriginals");
-        t1.setPopulation(10000);
-        t1.setFire_xp(70);
-
-        t1.toConsole();
-    }
+	 public static void main(String [] args)
+	    {
+	        Tribe t1 = new Tribe();
+	        t1.setName("Aboriginals");
+	        t1.setPopulation(982);
+	        t1.setFire_xp(72);
+	        t1.toConsole();
+	        
+	        Tribe t2 = new Tribe();
+	        t2.setName("Métis");
+	        t2.setPopulation(280);
+	        t2.setFire_xp(25);
+	        t2.toConsole();
+	    }
 }
--- Object Oriented Programming/Tribe.java
+++ Object Oriented Programming/Tribe.java
@@ -1,51 +1,47 @@
-package labwork1;
-public class Tribe
+package Labwork1;
+public class Tribe 
 {
-    private String name;
-    private int population;
-    private int fire_xp;
+	 private String name;
+	 private int population;
+	 private int fire_xp;
 
-    //!     Getters
+	 //!     Getters
+	 public String getName()             //? Gets tribe name
+	 { 
+	     return name;
+	 }
+     public int getPopulation()          //? Gets tribe population 
+     {
+     	return population;
+     }
 
-    public String getName()             //? Gets tribe name
-    { 
-        return name;
-    }
+	 public int getFire_xp()             //? Gets tribe experience 
+	 {
+	     return fire_xp;
+	 }
 
-    public int getPopulation()          //? Gets tribe population 
-    {
-        return population;
-    }
+	 //!       Setters
 
-    public int getFire_xp()             //? Gets tribe experience 
-    {
-        return fire_xp;
-    }
+	 public void setName(String new_name)        //?  Sets tribe name
+	 {
+	     name = new_name;
+	 }
+	 public void setPopulation(int new_population) //?  Gets tribe population
+	 {
+	     population = new_population;
+	 }
 
-    //!       Setters
+	 public void setFire_xp(int new_fire_xp)         //? Gets tribe experience
+	 {
+	     fire_xp = new_fire_xp;
+	 }
 
-    public void setName(String new_name)        //?  Sets tribe name
-    {
-        name = new_name;
-    }
-
-    public void setPopulation(int new_population) //?  Gets tribe population
-    {
-        population = new_population;
-    }
-
-    public void setFire_xp(int new_fire_xp)         //? Gets tribe experience
-    {
-        fire_xp = new_fire_xp;
-    }
-
-    //!       Console Print
-    public void toConsole()
-    {
-        System.out.println("Tribe Name: " + name);
-        System.out.println("Tribe population: " + population);
-        System.out.println("Fire Experience: " + fire_xp +"%");
-
-    }
+	 //!       Console Print
+	 public void toConsole()
+	 {
+	     System.out.println("Название племени: " + name);
+	     System.out.println("Население племени: " + population);
+	     System.out.println("Знакомство с огнем: " + fire_xp +"%");
+     }
 
 }
