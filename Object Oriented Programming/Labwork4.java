2bfea119909291e98e9c4bf22e431bd2a1643f15
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-10-06 18:15:39 +0300

    Lab 4

--- Object Oriented Programming/Labwork4.java
+++ Object Oriented Programming/Labwork4.java
@@ -1,86 +1,169 @@
 package Labwork4;
-import java.util.Scanner;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-//package Labwork4;
-import java.io.*;
 public class Labwork4 
 {
-	static void matrixKeyboard() 
-	{
-		int n = 5;
-		int m = 5;
-		int[][] arr = new int[n][m];
-		try (Scanner in = new Scanner(System.in))
-		{
-			arr = new int[n][m];
-			System.out.println("Enter the elements of the matrix: "); 
-			for (int i = 0; i < n; i++) 
-			{
-				for (int j = 0; j < m; j++) 
-				{
-					arr[i][j] = in.nextInt();
-				}
-			}
-		}
-		System.out.print("Модифицированный массив: "); 
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++) 
-			{
-				if(i % 5 == 0)
-					System.out.println(" ");
-				System.out.print(arr[i]+ ", ");
-			}
-			
-		}
-		
-	}
-//	static void matrixFile()
-//	{
-//		String path = "test.txt";
-//		int[][] arr = null;
-//		try 
-//		{
-//			try (Scanner in = new Scanner(new File(path)))
-//			{
-//				n = in.nextInt();
-//				m = in.nextInt();
-//				arr = new int[n][m];
-//				for (int i = 0; i < n; i++) {
-//					for (int j = 0; j < m; j++) {
-//						arr[i][j] = in.nextInt();
-//					}
-//				}
-//			}
-//		} catch (FileNotFoundException e) {
-//		System.out.println("Ошибка " + e + "! файл " +
-//		path + " не найден!");
-//		} catch (InputMismatchException e) {
-//		System.out.println("Ошибка " + e + "! Не
-//		соответствие типов данных!");
-//		}
-//	}
-public static void main (String args[]) 
+	public static final int NUM = 5;
+	public static final int NUMM = 5;
+	public static Scanner sc = new Scanner(System.in);
+	public static int[][] arr = new int[NUM][NUMM];
+	public static int [] X = {-1, -1, -1, -1, -1};
+	
+	//Method for asking user
+	static void query()
 	{
 		int fromFile = 1;
 		int fromKeyboard = 2;
 		int choice;
-		try(Scanner in = new Scanner(System.in))
-		{
-			System.out.println("Press 1 to enter matrix from file; "
-					+ "Press 2 to enter matrix from keyboard: ");
-			choice = in.nextInt();
-		}
-		  
+		System.out.println("Нажмите 1, чтобы ввести матрицу из файла;\n" + 
+				"Нажмите 2, чтобы ввести матрицу с клавиатуры:");
+			choice = sc.nextInt();
+					  
         if (choice == fromFile)
-        	System.out.println("File input");
+        {
+        	System.out.println("Матрица из файла");
+        	read_from_file();
+        }
         if (choice == fromKeyboard)
-        	matrixKeyboard();
+        	{
+        	System.out.println("Матрица с клавиатуры");
+        	read_from_keyboard();
+        	}
         if (choice != fromFile && choice != fromKeyboard)
-            System.out.println("wrong input");
+            System.out.println("неверный ввод!!");
+	}
+	
+	// Method for reading from file	
+	public static int[][] read_from_file() {
+		
+		try {
+			try (Scanner in = new Scanner(new File("data.txt"))) {
+
+				
+				for (int i = 0; i < NUM; i++) {
+					for (int j = 0; j < NUMM; j++) {
+						arr[i][j] = in.nextInt();
+						}
+					}
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println("Ошибка " + e + "! файл " + "data.txt" + " не найден!");
+				} catch (InputMismatchException e) {
+					System.out.println("Ошибка " + e + "! Не соответствие типов данных!");
+					}
+
+		return arr;
+		}
+
+	// Method for reading matrix from keyboard
+	static void read_from_keyboard() 
+	{
+		
+			System.out.println("Введите 25 чисел: "); 
+			for (int i = 0; i < NUM; i++)
+			{
+				for (int j = 0; j < NUMM; j++) 
+				{
+					
+						arr[i][j] = sc.nextInt();
+					
+				}
+			}
+				
+			}
+	
+	// method for displaying on console
+	static void display()
+		{
+		System.out.print("Начальная матрица"); 
+			for (int i = 0; i < NUM; i++)
+			{
+				for (int j = 0; j < NUMM; j++)
+				{
+					if(j % 5 == 0)
+					{
+						System.out.println(" ");
+					}
+					System.out.print(arr[i][j]+ ", ");
+				
+				}
+			 
+			}
+		}
+
+	// Method for determining X
+	static void calc()
+	{
+		int min = arr[0][0];  
+        for (int i = 0; i < arr.length; i++) 
+        {
+        	for (int j = 0; j < arr.length; j++) 
+             
+           if(arr[i][j] < min)  
+               min = arr[i][j]; 
+        }  
+        for (int i = 0; i < arr.length; i++) 
+        {
+        	for (int j = 0; j < arr.length; j++) 
+        	{
+        		if(arr[i][j] == min)  
+        		{
+        			X[i] = 1;
+        		}
+        	}
+        }
+        System.out.print("\nМассив X: ");
+        for (int i = 0; i < 5; i++)
+		{
+			System.out.print(X[i]+ ", ");
+		
+		}
+	}
+	
+	//Method for displaying Array X
+	static void calc_display()
+	{
+		 System.out.print("\nМассив X: ");
+	        for (int i = 0; i < 5; i++)
+			{
+				System.out.print(X[i]+ ", ");
+			
+			}
+	}
+
+	// Method for writing Array X to file
+	static void write_to_file()
+	{
+		
+	try {
+		PrintStream print = new PrintStream(new FileOutputStream("arrayX.txt"));
+		System.setOut(print);
+		System.out.print("Массив X: ");
+        for (int i = 0; i < 5; i++)
+		{
+			System.out.print(X[i]+ ", ");
+		
+		}
+		print.close();
+		} catch(FileNotFoundException e) 
+		{
+			e.printStackTrace();		
+		}
+	}
+	
+	//Main port
+	public static void main (String args[]) 
+	{		
+        query();
+		display();
+		calc();
+//		calc_display();
+		write_to_file();
 	}
 }
 	
