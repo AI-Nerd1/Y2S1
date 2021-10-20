8cea42e3bbcea368f28dd1b44340debcbe14d091
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-10-20 20:52:40 +0300

    Add files via upload

--- EBM/EBM6.py
+++ EBM/EBM6.py
@@ -2,20 +2,12 @@
 dec_val= int(input("Введите десятичное число: "))
 bin_val = int(input("Введите двоичное число: "))
 
-# Function to convert decimal number to binary using recursion
 def DecimalToBinary(num):
 	if num >= 1:
 		DecimalToBinary(num // 2)
     
 	print(num % 2, end = '')
-# Driver Code
-if __name__ == '__main__':
-	# decimal value
-    DecimalToBinary(dec_val)
-print(f"(2) = {dec_val}(10) ")
-	# Calling function
 
-# Function calculates the decimal equivalent of a given binary number
 
 def binaryToDecimal(bin_val):
 	
@@ -27,14 +19,10 @@ def binaryToDecimal(bin_val):
 		bin_val = bin_val//10
 		i += 1
 	print(decimal)
-	
 
-# Driver code
-if __name__ == '__main__':
-	binaryToDecimal(bin_val)
-print(f"(10) = {bin_val}(2) ")
-	# Calling function
 print("\n")
-	
-
-    
+print(dec_val,"а двоичную систему: ", end = '')
+DecimalToBinary(dec_val)
+print("")
+print(bin_val,"на десятичную систему ", end ='')
+binaryToDecimal(bin_val) 
